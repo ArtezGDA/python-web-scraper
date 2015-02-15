76037941ec15e6b12be5801f04f51c9fcec20369
irlabs <developer@irlabs.nl>
2015-02-15 12:55:46 +0100

    Basic program structure

---
+++ wiki_scraper.py
@@ -0,0 +1,15 @@
+"""
+wiki_scraper.py
+
+Simple scraper that pulls multiple pages from wikipedia.
+"""
+
+def get_all_movies():
+	return []
+
+def main():
+	movies = get_all_movies()
+	print movies
+	
+if __name__ == '__main__':
+	main()
