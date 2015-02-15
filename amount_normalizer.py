77d04545d8ba4e25df376af66b1fe91c097216d9
irlabs <developer@irlabs.nl>
2015-02-15 16:40:30 +0100

    Finished normalize module

--- amount_normalizer.py
+++ amount_normalizer.py
@@ -5,7 +5,35 @@ Script that normalizes the amount string to an integer number in USD
 """
 
 def normalize_amount(stringInput):
-	return 0
+	#
+	# Only if the amount starts with a '$'
+	if stringInput.startswith('$'):
+		
+		# Strip of the '$'
+		stringInput = stringInput.strip('$')
+		
+		# Get rid of all comma's
+		stringInput = stringInput.replace(',','')
+		
+		# Calculate the multiplier
+		multiplier = 1
+		
+		# million
+		if stringInput.endswith('million'):
+			multiplier = 1000000
+			stringInput = stringInput.strip('million')
+
+		# billion
+		if stringInput.endswith('billion'):
+			multiplier = 1000000000
+			stringInput = stringInput.strip('billion')
+			
+		# print stringInput + " " + str(multiplier)
+		amount = int(round(float(stringInput) * multiplier))
+		return amount
+		
+	else:
+		return None
 
 def main():
 	"""
@@ -13,7 +41,9 @@ def main():
 	"""
 	testfile = open('box_office_amounts.txt', 'r')
 	for line in testfile.readlines():
-		print line
+		# Strip white space around the line (in this case the newline \n )
+		test_text = line.strip()
+		print normalize_amount(test_text)
 	testfile.close()
 	
 	
